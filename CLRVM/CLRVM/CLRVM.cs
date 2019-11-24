caf3e1fe57b46d504652d23ac0d49969067f4e7c
mr7falcon <mr.falcon.inc@gmail.com>
2019-11-24 16:34:25 +0300

    CLR DLL UDF

---
+++ CLRVM/CLRVM/CLRVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using System.Data.SqlTypes;
+
+namespace CLRVM
+{
+    public class CLRVM
+    {
+        [DllImport("VirtualMachine_x64.dll", EntryPoint = "VMRun", CallingConvention = CallingConvention.StdCall)]
+        public static extern bool VMRun(byte[] program);
+
+        public static void VMRunUDF(SqlBinary byteCode)
+        {
+            VMRun(byteCode.Value);
+        }
+    }
+}
